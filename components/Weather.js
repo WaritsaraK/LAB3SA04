8c9ca64fc6c816719d98d7035e00de9c86d86246
WaritsaraK <6310110443@psu.ac.th>
2022-08-27 13:40:28 +0700

    Part Flex Box

--- components/Weather.js
+++ components/Weather.js
@@ -4,26 +4,39 @@ import Forecast from "./Forecast";
 
 export default function Weather(props) {   
     const [forecastInfo, setForecastInfo] = useState({         
-        main: '-',         
-        description: '-',         
+        main: 'main',         
+        description: 'description',         
         temp: 0     
     })  
 
     return (       
         <View>             
             <ImageBackground source={require('../backgroundd.jpg')} style={styles.backdrop}>                 
-                <Text>Zip Code</Text>                 
-                <Text>{props.zipCode}</Text>                 
-                <Forecast {...forecastInfo} />             
+                <View style={styles.background}>
+                    <Text style={styles.zipCodeText}>Zip Code is {props.zipCode}</Text>
+                    <Forecast {...forecastInfo} />
+                </View>             
             </ImageBackground>         
         </View>    
     )
 } 
 
 const styles = StyleSheet.create({     
-    backdrop: {         
-        alignItems: 'center',         
-        width: '100%',         
-        height: '100%'     
-    }, 
-}); 
+    backdrop: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100%',
+        height: '100%',
+    },
+    zipCodeText:{
+        paddingTop: 30,
+        color:'white',
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    background:{
+        width: '100%',
+        height: '35%',
+        backgroundColor: 'rgba(0, 0, 50, 0.35)'
+    }
+})
